a8939ea1bb744dd46ec8562ec277c902723cb5bb
sjww0306 <sjww03@gmail.com>
2026-01-12 17:07:27 +0900

    first commit

---
+++ src/main/java/org/example/develop_server_kcompany/DevelopServerKcompanyApplication.java
@@ -0,0 +1,13 @@
+package org.example.develop_server_kcompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopServerKcompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevelopServerKcompanyApplication.class, args);
+	}
+
+}
